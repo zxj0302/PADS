ebb76a0b97bc5cd13aa8aae76a4e63a16dd2e502
zhuxiangju <zhuxiangjv@gmail.com>
2025-01-03 16:46:25 +0800

    init new rep

--- test.py
+++
@@ -1,5 +0,0 @@
-from src import create_heatmap
-
-datasets = ['Brexit', 'Referendum_', 'Gun', 'Abortion', 'Election', 'Partisanship']
-for d in datasets[:1]:
-    create_heatmap(f'Output/{d}/graph.gml', f'Output/{d}/diffusion.json', save_path=f'figs/heatmap/{d}.pdf')
